45b6925923610ee5674a8aaf4bb1af114b8b433a
Rails Test App <faisallarai@gmail.com>
2016-08-26 10:27:51 +0000

    add user listing and show update

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -27,6 +27,7 @@ class ArticlesController < ApplicationController
   end
 
   def update
+    @article.user = User.last
     if @article.update (article_params)
       flash[:success] = "Article was successfully updated"
       redirect_to article_path(@article)
